8cb4d990e28432e4b420aeddd43df88036058d2c
Manuel Quintero Cubillos <mquinteroc@psl.com.co>
2020-12-16 09:44:08 -0500

    new test organization & corrections

--- test/GithubApi.Repositories.test.js
+++ test/GithubApi.Repositories.test.js
@@ -8,73 +8,95 @@ chai.use(chaiSubset);
 
 const { expect } = chai;
 
-let userInfo = null;
 const urlBase = 'https://api.github.com';
 const githubUserName = 'aperdomob';
 const repositoryName = 'jasmine-awesome-report';
-const md5ReadMe = '0e62b07144b4fa997eedb864ff93e26b';
 
 describe('Github Api Test', () => {
-  describe('Users & Repositories', () => {
-    before(async () => {
-      userInfo = await agent.get(`${urlBase}/users/${githubUserName}`)
-        .auth('token', process.env.ACCESS_TOKEN)
-        .set('User-Agent', 'agent');
-    });
-
-    it('Via GET verb obtain user info', async () => {
-      expect(userInfo.status).to.equal(statusCode.OK);
-      expect(userInfo.body.name).equal('Alejandro Perdomo');
-      expect(userInfo.body.company).equal('PSL');
-      expect(userInfo.body.location).equal('Colombia');
-    });
-
-    it('Via GET verb obtain user repositories info', async () => {
-      const response = await agent.get(userInfo.body.repos_url)
-        .auth('token', process.env.ACCESS_TOKEN)
-        .set('User-Agent', 'agent');
-
-      const repository = response.body.find((element) => element.name === repositoryName);
-      expect(response.status).to.equal(statusCode.OK);
-      expect(repository.full_name).to.equal(`${githubUserName}/${repositoryName}`);
-      expect(repository.private).to.equal(false);
-      expect(repository.description).to.equal('An awesome html report for Jasmine');
-    });
-
-    it('Via GET verb download jasmine-awesome-report repository', async () => {
-      const response = await agent.get(`${urlBase}/repos/${githubUserName}/${repositoryName}/zipball`)
-        .auth('token', process.env.ACCESS_TOKEN)
-        .set('User-Agent', 'agent');
-
-      expect(response.status).to.equal(statusCode.OK);
-      expect(response.header['content-type']).to.equal('application/zip');
-    });
-
-    it('Via GET verb obtain jasmine-awesome-report README.md info', async () => {
-      const response = await agent.get(`${urlBase}/repos/${githubUserName}/${repositoryName}/contents`)
-        .auth('token', process.env.ACCESS_TOKEN)
-        .set('User-Agent', 'agent');
-
-      const readme = response.body.find((element) => element.name === 'README.md');
-      expect(readme).to.containSubset({
-        path: 'README.md',
-        sha: 'b9900ca9b34077fe6a8f2aaa37a173824fa9751d'
+  describe('Scenario: Users & Repositories', () => {
+    describe(`Given a Github account like ${githubUserName}`, () => {
+      let userInfo;
+      describe(`When a GET request is used to retrieve ${githubUserName} information`, () => {
+        before(async () => {
+          userInfo = await agent.get(`${urlBase}/users/${githubUserName}`)
+            .auth('token', process.env.ACCESS_TOKEN)
+            .set('User-Agent', 'agent');
+        });
+        it(`Then ${githubUserName}'s name, company and location are expected`, () => {
+          expect(userInfo.status).to.equal(statusCode.OK);
+          expect(userInfo.body.name).equal('Alejandro Perdomo');
+          expect(userInfo.body.company).equal('PSL');
+          expect(userInfo.body.location).equal('Colombia');
+        });
       });
-    });
 
-    it('Via GET verb download jasmine-awesome-report README.md and check MD5 hash', async () => {
-      const contents = await agent.get(`${urlBase}/repos/${githubUserName}/${repositoryName}/contents`)
-        .auth('token', process.env.ACCESS_TOKEN)
-        .set('User-Agent', 'agent');
+      describe(`When a GET request is used to retrieve ${githubUserName}'s ${repositoryName} repository information`, () => {
+        let response;
+        let repository;
+        before(async () => {
+          response = await agent.get(userInfo.body.repos_url)
+            .auth('token', process.env.ACCESS_TOKEN)
+            .set('User-Agent', 'agent');
+          repository = response.body.find((element) => element.name === repositoryName);
+        });
 
-      const readme = contents.body.find((element) => element.name === 'README.md');
-      const response = await agent.get(readme.download_url)
-        .auth('token', process.env.ACCESS_TOKEN)
-        .set('User-Agent', 'agent');
+        it(`Then ${repositoryName}'s full name, private and description are expected`, () => {
+          expect(response.status).to.equal(statusCode.OK);
+          expect(repository.full_name).to.equal(`${githubUserName}/${repositoryName}`);
+          expect(repository.private).to.equal(false);
+          expect(repository.description).to.equal('An awesome html report for Jasmine');
+        });
+      });
 
-      const md5FileResponse = md5(response.text);
+      describe(`When a GET request is used to download ${githubUserName}'s ${repositoryName} repository`, () => {
+        let response;
+        before(async () => {
+          response = await agent.get(`${urlBase}/repos/${githubUserName}/${repositoryName}/zipball`)
+            .auth('token', process.env.ACCESS_TOKEN)
+            .set('User-Agent', 'agent');
+        });
 
-      expect(md5FileResponse).to.equal(md5ReadMe);
+        it('Then the response must return a status code 200, and the content type must be a zip', () => {
+          expect(response.status).to.equal(statusCode.OK);
+          expect(response.header['content-type']).to.equal('application/zip');
+        });
+      });
+
+      describe(`When a GET request is used to retrieve ${githubUserName}'s ${repositoryName} README.md info`, () => {
+        let contents;
+        let readme;
+        before(async () => {
+          contents = await agent.get(`${urlBase}/repos/${githubUserName}/${repositoryName}/contents`)
+            .auth('token', process.env.ACCESS_TOKEN)
+            .set('User-Agent', 'agent');
+
+          readme = contents.body.find((element) => element.name === 'README.md');
+        });
+
+        it('Then the README.md info must return a path, and it\'s sha', () => {
+          expect(readme).to.containSubset({
+            path: 'README.md',
+            sha: 'b9900ca9b34077fe6a8f2aaa37a173824fa9751d'
+          });
+        });
+
+        describe(`When a GET request is used to download ${githubUserName}'s ${repositoryName} README.md and it's MD5 is calculated`, () => {
+          let response;
+          let md5FileResponse;
+          const md5ReadMe = '0e62b07144b4fa997eedb864ff93e26b';
+          before(async () => {
+            response = await agent.get(readme.download_url)
+              .auth('token', process.env.ACCESS_TOKEN)
+              .set('User-Agent', 'agent');
+
+            md5FileResponse = md5(response.text);
+          });
+
+          it('Then the README.md MD5 calculated and the one calculated before most be equal', () => {
+            expect(md5FileResponse).to.equal(md5ReadMe);
+          });
+        });
+      });
     });
   });
 });
