f5712d45d2e8a58a1924c08d1f0dd84ee4ada37f
Manuel Quintero Cubillos <mquinteroc@psl.com.co>
2020-12-17 16:19:52 -0500

    Github testing response time

---
+++ test/GithubApi.Users.test.js
@@ -0,0 +1,30 @@
+const agent = require('superagent');
+const statusCode = require('http-status-codes');
+const responseTime = require('superagent-response-time');
+const { expect } = require('chai');
+
+const urlBase = 'https://api.github.com';
+
+describe('Github Api Test', () => {
+  describe('Scenario: Response Time', () => {
+    describe('Given a Github account', () => {
+      describe('When a GET request is used to retrieve all users', () => {
+        let getAllResponse;
+        let respTime;
+        before(async () => {
+          getAllResponse = await agent.get(`${urlBase}/users`)
+            .auth('token', process.env.ACCESS_TOKEN)
+            .set('User-Agent', 'agent')
+            .use(responseTime((callback, time) => {
+              respTime = time;
+            }));
+        });
+
+        it('Then the delay of the response must be below 5000ms', () => {
+          expect(getAllResponse.status).to.equal(statusCode.OK);
+          expect(respTime).to.be.below(5000);
+        });
+      });
+    });
+  });
+});
