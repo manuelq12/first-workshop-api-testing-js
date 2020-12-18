f8e2e44761ffedd96f034df9a3daf80857d095fd
Manuel Quintero Cubillos <mquinteroc@psl.com.co>
2020-12-17 22:35:05 -0500

    query-parameters testing done

--- test/GithubApi.Users.test.js
+++ test/GithubApi.Users.test.js
@@ -24,6 +24,40 @@ describe('Github Api Test', () => {
           expect(getAllResponse.status).to.equal(statusCode.OK);
           expect(respTime).to.be.below(5000);
         });
+
+        it('And the response must  have a maximum of 30 users', () => {
+          expect(getAllResponse.body.length).to.be.equal(30);
+        });
+      });
+
+      describe('When a GET request is used to retrieve only 10 users', () => {
+        let tenUsersResponse;
+        const query = { per_page: 10 };
+        before(async () => {
+          tenUsersResponse = await agent.get(`${urlBase}/users`)
+            .auth('token', process.env.ACCESS_TOKEN)
+            .set('User-Agent', 'agent')
+            .query(query);
+        });
+
+        it('Then the response must contain only 10 users', () => {
+          expect(tenUsersResponse.body.length).to.be.equal(10);
+        });
+      });
+
+      describe('When a GET request is used to retrieve only 50 users', () => {
+        let fiftyUsersResponse;
+        const query = { per_page: 50 };
+        before(async () => {
+          fiftyUsersResponse = await agent.get(`${urlBase}/users`)
+            .auth('token', process.env.ACCESS_TOKEN)
+            .set('User-Agent', 'agent')
+            .query(query);
+        });
+
+        it('Then the response must contain only 50 users', () => {
+          expect(fiftyUsersResponse.body.length).to.be.equal(50);
+        });
       });
     });
   });
